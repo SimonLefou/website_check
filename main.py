2faa7cf9dadf07d6a1cb3e3b630ef531231b8b63
SimonLefou <simonlefou@protonmail.com>
2022-05-31 13:18:04 +0200

    better Exceptions handling

--- main.py
+++ main.py
@@ -2,12 +2,6 @@ import requests
 import pandas as pd
 
 
-excel_file = pd.read_excel("liste_site.xlsx", header=None)
-col1 = [website for website in excel_file[0] if type(website) == str]   # Recupération des Websites en colonne
-nbre_url = len(col1)
-LISTE_EXTENSION = ["com", "fr"]
-
-
 def convert(url):
     if url.startswith('http://www.'):
         return 'https://www.' + url[len('http://www.'):]
@@ -21,7 +15,7 @@ def convert(url):
 
 
 def clean_url(url):
-    url_copy = url
+    url_copy = url[:]
     url = url.split("/")
     url = url[-1].split(".")
     if url[-1] not in LISTE_EXTENSION:
@@ -34,33 +28,46 @@ def clean_url(url):
     return url
 
 
-def is_down(url):
+def get_status(url):
     try:
-        reponse = requests.get(url)
-        print(f"Status Code : {reponse.status_code}")
-    except:
-        print(f"{url} = non disponible")
-        return True
-    return False
+        response = requests.get(url)
+        if int(response.status_code / 100) == 2:
+            return True
+        else:
+            return False
+    except Exception:
+        return False
 
 
+try:
+    excel_file = pd.read_excel("liste_site.xlsx", header=None)
+except FileNotFoundError as e:
+    print("Erreur : le fichier liste_site.xlsx n'a pas pu être ouvert. Assurez-vous qu'il soit dans le même répertoire")
+except Exception as e:
+    print(e)
+else:
+    col1 = [website for website in excel_file[0] if type(website) == str]   # Recupération des Websites en colonne
+
+nbre_url = len(col1)
+LISTE_EXTENSION = ["com", "fr"]
 file = open("Liste_Sites_Down.txt", "w+")
-
 
 for i in range(len(col1)):
     check_url = col1[i]
     check_url = convert(check_url)
     check_url = clean_url(check_url)
-    print(check_url)
 
-    if is_down(check_url):
+    response = get_status(check_url)
+    print(f"{check_url}, {response}")
+    if response:
         file.write(check_url)
         file.write("\n")
-    print("Programme en cours")
-    print(f"{int(i / nbre_url * 100)}%")
+        print("Programme en cours")
+        print(f"{int(i / nbre_url * 100)}%")
 
 print("Fin d'execution")
 file.close()
+k = input("press close to exit")
 
 
 """
